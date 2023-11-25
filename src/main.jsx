cae9913babac5af3369f2ec45886b7649dc8364a
sovonbd <sovonsdp@gmail.com>
2023-11-25 16:28:44 -0600

    updated the resource files

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-<<<<<<< HEAD
-import Routes from "./Routes/Routes";
-=======
-import Routes from "./routes/Routes";
->>>>>>> 265cc67 (updated the login and register page newly)
+
 import { ThemeProvider } from "@mui/material";
 import theme from "./Theme/Theme";
+import Routes from "../src/Routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
