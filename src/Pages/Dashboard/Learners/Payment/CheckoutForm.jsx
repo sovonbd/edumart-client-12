b9d41d6dc3a3b4f15acff293f7f5bf422259e5d2
sovonbd <sovonsdp@gmail.com>
2023-11-29 19:49:32 -0600

    updated the payment system

--- src/Pages/Dashboard/Learners/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Learners/Payment/CheckoutForm.jsx
@@ -1,19 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 import { useParams } from "react-router-dom";
 import {
   CardElement,
   PaymentElement,
+  useElements,
   useStripe,
 } from "@stripe/react-stripe-js";
 import { Button } from "@mui/material";
+import useAuth from "../../../../hooks/useAuth";
 
 const CheckoutForm = () => {
-  // const [clientSecret, clientSecret] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+  const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
   const stripe = useStripe();
+  const elements = useElements();
+  const [error, setError] = useState("");
+  const [transactionId, setTransactionId] = useState("");
+
   const { id } = useParams();
+
   const { data: price } = useQuery({
     queryKey: ["price"],
     queryFn: async () => {
@@ -21,15 +29,66 @@ const CheckoutForm = () => {
       return res.data.price;
     },
   });
+  console.log(typeof price);
+
+  useEffect(() => {
+    axiosSecure.post("/create-payment-intent", { price: price }).then((res) => {
+      console.log(res.data.clientSecret);
+      setClientSecret(res.data.clientSecret);
+    });
+  }, [price, axiosSecure]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const card = elements.getElement(CardElement);
+
+    if (card === null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("error", error);
+      setError(error.message);
+    } else {
+      console.log("payment method", paymentMethod);
+      setError("");
+    }
+
+    // confirm payment by card
+
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: user?.displayName || "annonymous",
+            email: user?.email || "annonymous",
+          },
+        },
+      });
+    if (confirmError) {
+      console.log("confirm error", error);
+    } else {
+      console.log("payment Intent", paymentIntent);
+      if (paymentIntent.status === "succeeded")
+        setTransactionId(paymentIntent.id);
+    }
   };
   console.log(price);
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <div>
+        <h3 className="lg:text-4xl font-bold">Payment Details</h3>
+        <p>Enter your card information below for this payment</p>
+        <p>Price: ${price}</p>
+      </div>
+      <form onSubmit={handleSubmit} className="w-1/2">
         <CardElement
           options={{
             style: {
@@ -45,10 +104,16 @@ const CheckoutForm = () => {
               },
             },
           }}
+          className="m-6 p-2 border"
         />
-        <Button variant="contained" type="submit" disabled={!stripe}>
+        <Button
+          variant="contained"
+          type="submit"
+          disabled={!stripe || !clientSecret}>
           Pay
         </Button>
+        <p className="text-red-600">{error}</p>
+        <p className="text-green-600">Your transaction id: {transactionId}</p>
       </form>
     </div>
   );
--- src/Pages/Dashboard/Learners/Payment/Payment.jsx
+++ src/Pages/Dashboard/Learners/Payment/Payment.jsx
@@ -9,10 +9,7 @@ const Payment = () => {
 
   return (
     <div>
-      <div>
-        <h3 className="lg:text-4xl font-bold">Payment Details</h3>
-        <p>Enter your card information below for this payment</p>
-      </div>
+      
       <div>
         <Elements stripe={stripePromise}>
           <CheckoutForm></CheckoutForm>
