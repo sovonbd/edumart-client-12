c212540f6b803a8931428b172c0b7e11c8b4a68c
MR. Sovon <sovonsdp@gmail.com>
2024-05-15 00:27:21 -0500

    update

--- src/components/ReturnPortfolio/ReturnPortfolio.jsx
+++ src/components/ReturnPortfolio/ReturnPortfolio.jsx
@@ -1,9 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { HiOutlineArrowLongRight } from "react-icons/hi2";
 
 const ReturnPortfolio = () => {
+  const location = useLocation();
   return (
-    <div className="bg-black p-2 z-90  text-white text-center fixed w-full">
+    <div
+      className={`bg-black p-2  text-white text-center fixed w-full ${
+        location.pathname.startsWith("/dashboard") ? "z-90 md:w-11/12" : "z-10"
+      }`}>
       <Link
         to="https://sovon-10e71.web.app/"
         className="flex items-center justify-center gap-1">
